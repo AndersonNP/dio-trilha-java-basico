60c3fb5cac7af8e3139fb95feed7bea6260d06b1
Anderson Nascimento <anderson.nascimento.info@gmail.com>
2024-06-19 16:21:39 -0300

    Condição Ternária

--- CONTROLE-FLUXO/src/ResultadoEscolar.java
+++ CONTROLE-FLUXO/src/ResultadoEscolar.java
@@ -1,13 +1,10 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 6;
+        int nota = 10;
 
-        if(nota >= 7){
-            System.out.println("Aprovado");
-        }else if(nota >= 5 && nota < 7){
-            System.out.println("Recuperação");
-        }else{
-            System.out.println("Reprovado");
-        }
+        String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Recuperação" : "Reprovado";
+      
+            System.out.println(resultado);
+        
     }
 }
