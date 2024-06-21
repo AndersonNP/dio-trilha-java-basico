63f9f967da4ecbe43e443906635cd89e06fcb233
Anderson Nascimento <anderson.nascimento.info@gmail.com>
2024-06-21 16:03:56 -0300

    Enums

---
+++ tipos-variaveis/src/EstadoBrasileiro.java
@@ -0,0 +1,27 @@
+public enum EstadoBrasileiro {
+    SAO_PAULO("SP", "São Paulo"),
+    RIO_JANEIRO("RJ","Rio de Janeiro"),
+    PIAUI("PI","Piauí"),
+    MARANHAO("MA","Maranhão"),
+    CEARA("CE", "Ceará");
+
+    private String nome;
+    private String sigla;
+
+    private EstadoBrasileiro(String sigla, String nome){
+        this.sigla = sigla;
+        this.nome = nome;
+    }
+
+    public String getSigla(){
+        return this.sigla;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public String getNomeMaiusculo(){
+        return this.nome.toUpperCase();
+    }
+}
